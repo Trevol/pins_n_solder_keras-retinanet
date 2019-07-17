65943dffca44583d5dfa52f2e3fe9fd9f2d0c0d9
trevol <iamvovan@mail.ru>
2019-07-17 13:32:15 +0300

    playback states refactoring

--- utils/VideoController.py
+++ utils/VideoController.py
@@ -3,9 +3,13 @@ from .KbdKeys import KbdKeys
 
 
 class VideoController:
+    class States:
+        PAUSE = 'pause'
+        PLAYING = 'playing'
+
     def __init__(self, delay, state=None):
         self.delay = delay
-        self.state = state or 'normal'
+        self.state = state or self.States.PLAYING
 
     def __cv_waitKey(self, delay=None):
         if delay is None:
@@ -13,25 +17,26 @@ class VideoController:
         return cv2.waitKey(delay) & 0xFF
 
     def __wait_pause_state(self):
-        if self.state != 'pause':
-            raise Exception('state != pause')
+        if self.state != self.States.PAUSE:
+            raise Exception(f'state != {self.States.PAUSE}')
+
         key = self.__cv_waitKey(-1)
         if key == KbdKeys.Q:
-            self.state = 'normal'
+            self.state = self.States.PLAYING
         return key
 
     def __wait_normal_state(self):
-        if self.state != 'normal':
-            raise Exception('state != normal')
+        if self.state != self.States.PLAYING:
+            raise Exception(f'state != {self.States.PLAYING}')
 
         key = self.__cv_waitKey()
         if key == KbdKeys.Q:
-            self.state = 'pause'
+            self.state = self.States.PAUSE
             return self.__wait_pause_state()
         return key
 
     def waitKey(self):
-        if self.state == 'pause':
+        if self.state == self.States.PAUSE:
             return self.__wait_pause_state()
         else:
             return self.__wait_normal_state()
