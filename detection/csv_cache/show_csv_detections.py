24ebed27944280615b14aaa30c45c8227d496040
trevol <iamvovan@mail.ru>
2019-07-21 16:30:16 +0300

    files as generator

--- detection/csv_cache/show_csv_detections.py
+++ detection/csv_cache/show_csv_detections.py
@@ -1,12 +1,21 @@
 import cv2
 from detection.csv_cache.DetectionsCSV import DetectionsCSV
 import utils.visualize
+from utils import resize
 from utils.VideoPlayback import VideoPlayback
 
 
 def csvToPcl():
     DetectionsCSV.csvToPickle('./data/detections_video6.csv', './data/detections_video6.pcl')
     DetectionsCSV.csvToPickle('./data/detections_video2.csv', './data/detections_video2.pcl')
+
+
+def files():
+    yield ('/HDD_DATA/Computer_Vision_Task/Video_6.mp4',
+           DetectionsCSV.loadPickle('./data/detections_video6.pcl'))
+
+    yield ('/HDD_DATA/Computer_Vision_Task/Video_2.mp4',
+           DetectionsCSV.loadPickle('./data/detections_video2.pcl'))
 
 
 def main():
@@ -17,21 +26,15 @@ def main():
         stateTitle = f'{winname} (FrameDelay: {frameDelay}, Autoplay: {autoplayLabel})'
         cv2.setWindowTitle(winname, stateTitle)
 
-    files = [
-        ('/HDD_DATA/Computer_Vision_Task/Video_6.mp4',
-         DetectionsCSV.loadPickle('./data/detections_video6.pcl')),
-
-        ('/HDD_DATA/Computer_Vision_Task/Video_2.mp4',
-         DetectionsCSV.loadPickle('./data/detections_video2.pcl'))
-    ]
-
     def frameReady(frame, framePos, playback):
         detections = framesDetections.get(framePos, [])
         utils.visualize.drawDetections(frame, detections)
         utils.visualize.putFramePos(frame, framePos)
+        if frame.shape[1] >= 1900:
+            frame = resize(frame, .8)
         cv2.imshow(winname, frame)
 
-    for sourceVideoFile, framesDetections in files:
+    for sourceVideoFile, framesDetections in files():
         videoPlayback = VideoPlayback(sourceVideoFile, 1, autoplayInitially=False)
         videoPlayback.play(onFrameReady=frameReady, onStateChange=indicatePlaybackState)
         videoPlayback.release()
