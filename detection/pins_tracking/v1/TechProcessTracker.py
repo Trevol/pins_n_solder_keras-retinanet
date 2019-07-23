e9b1635c2f48c7c7ac7109628c0fe6253551f423
trevol <iamvovan@mail.ru>
2019-07-23 22:06:09 +0300

    work on stable scenes tracking/detection

--- detection/pins_tracking/v1/TechProcessTracker.py
+++ detection/pins_tracking/v1/TechProcessTracker.py
@@ -1,77 +1,99 @@
 from detection.pins_tracking.v1.Box import Box
 import cv2
+from collections import deque
 
 
 # TODO: calc bounding box for stable Scene -
 class TechProcessTracker:
     def __init__(self):
         self.__stableRanges = []
-        self.__currentFrameRange = None
+        self.__currentScene = None
 
     def track(self, frameDetections, framePos, framePosMsec, frame):
         bboxes = [Box(d[0]) for d in frameDetections]
         self.__trackBoxes(bboxes, framePos, framePosMsec, frame)
 
     def __trackBoxes(self, bboxes, framePos, framePosMsec, frame):
-        if not self.__currentFrameRange:
+        if not self.__currentScene:
             if any(bboxes):
-                self.__currentFrameRange = FrameRange(bboxes, framePos, framePosMsec, frame)
+                self.__currentScene = StableScene(bboxes, framePos, framePosMsec, frame)
             return
 
-        wasUnstable = not self.__currentFrameRange.stable
+        wasUnstable = not self.__currentScene.stable
 
-        closeToRange = self.__currentFrameRange.addIfClose(bboxes, framePos, framePosMsec, frame)
+        closeToRange = self.__currentScene.addIfClose(bboxes, framePos, framePosMsec, frame)
 
-        if wasUnstable and self.__currentFrameRange.stable:
+        if wasUnstable and self.__currentScene.stable:
             # add to stable ranges IF range was unstable before addition new frame and become stable after
             self.__addCurrentRangeAsStable()
 
-        if not closeToRange:  # start new FrameRange
-            self.__currentFrameRange = FrameRange(bboxes, framePos, framePosMsec, frame)
+        if not closeToRange:
+            self.__currentScene = StableScene(bboxes, framePos, framePosMsec, frame)
 
     def __addCurrentRangeAsStable(self):
-        assert self.__currentFrameRange.stable
-        assert self.__currentFrameRange not in self.__stableRanges
+        assert self.__currentScene.stable
+        assert self.__currentScene not in self.__stableRanges
 
-        self.__stableRanges.append(self.__currentFrameRange)
+        self.__stableRanges.append(self.__currentScene)
         self.__logNewStableChanges()
 
     def __logNewStableChanges(self):
         assert any(self.__stableRanges)
 
         framePos, framePosMs, objectsCount = self.__stableRanges[-1].stats()
-        print(f'{framePos}, {framePosMs}, {objectsCount}')
+        print(f'{framePos}, {framePosMs:.0f}, {objectsCount}')
 
     def draw(self, img):
-        if self.__currentFrameRange and self.__currentFrameRange.stable:
-            self.__currentFrameRange.draw(img)
-        # if any(self.__stableRanges):  # draw last stable range
-        #     self.__stableRanges[-1].draw(img)
+        if self.__currentScene and self.__currentScene.stable:
+            self.__currentScene.draw(img)
 
 
-class FrameRange:
+class StableScene:
+    class Frames:
+        def __init__(self, stabilizationLenght):
+            self.first = None
+            self.recent = deque(maxlen=stabilizationLenght)
+
+        def stabilized(self):
+            return len(self.recent) == self.recent.maxlen
+
+        def append(self, frameInfo):
+            if not self.first:
+                self.first = frameInfo
+            self.recent.append(frameInfo)
+
+        def notEmpty(self):
+            return self.first is not None
+
+    ##############################################
+    __stabilizationLength = 5  # count of frames to ensure scene stability
+
     def __init__(self, bboxes, framePos, framePosMsec, frame):
-        self.__frames = []  # or collections.deque(maxlen = 50)
+        self.__frames = self.Frames(self.__stabilizationLength)
+        # self.__frames_old = []  # or collections.deque(maxlen = self.__stabilizationCount)
+
+        self.__instanceCount = None
         self.__meanBoxes = []
         self.addIfClose(bboxes, framePos, framePosMsec, frame)
 
     def stats(self):
         assert self.stable
-        first = self.__frames[0]
-        objectsCount = len(self.__meanBoxes)
-        return first.pos, first.posMsec, objectsCount
+        # first = self.__frames_old[0]
+        # objectsCount = len(self.__meanBoxes)
+        return self.__frames.first.pos, self.__frames.first.posMsec, self.__instanceCount
 
     @property
     def stable(self):
-        return len(self.__frames) >= 20
+        # return len(self.__frames_old) >= self.__stabilizationLength
+        return self.__frames.stabilized()
 
     def addIfClose(self, bboxes, framePos, framePosMsec, frame):
         if not any(bboxes):
             return False  # skip empty detections
 
-        if not any(self.__frames):
+        if not any(self.__frames.recent):
             self.__addToRange(FrameInfo(bboxes, framePos, framePosMsec, frame))
-            return True  # first frame belong to Range
+            return True  # first frame starts scene - so always belong to scene
 
         closeToRange, bboxes = self.__checkCloseToRange(bboxes)
         if closeToRange:
@@ -81,7 +103,7 @@ class FrameRange:
     ####################################
     def __checkCloseToRange(self, boxes):
         assert any(self.__meanBoxes)
-        if len(boxes) != len(self.__meanBoxes):
+        if len(boxes) < len(self.__meanBoxes):
             return False, None
 
         # detection close to mean boxes
@@ -108,23 +130,25 @@ class FrameRange:
             cv2.circle(img, tuple(meanBox.center), r, green, -1)
 
     def __addToRange(self, frameInfo):
+        # self.__frames_old.append(frameInfo)
         self.__frames.append(frameInfo)
         self.__recalcStatistics()
 
     def __recalcStatistics(self):
-        assert any(self.__frames)
+        assert self.__frames.notEmpty()
 
-        if len(self.__frames) == 1:
-            self.__meanBoxes = list(self.__frames[0].bboxes)
+        if len(self.__frames.recent) == 1:
+            self.__meanBoxes = list(self.__frames.first.bboxes)
+            self.__instanceCount = len(self.__meanBoxes)
             return
 
-        countOfInstances = len(self.__frames[0].bboxes)
-        for instanceIndex in range(countOfInstances):
-            instanceBoxesAcrossFrames = [frameInfo.bboxes[instanceIndex] for frameInfo in self.__frames]
+        for instanceIndex in range(self.__instanceCount):
+            instanceBoxesAcrossFrames = [frameInfo.bboxes[instanceIndex] for frameInfo in self.__frames.recent]
             instanceMeanBox = Box.meanBox(instanceBoxesAcrossFrames)
             self.__meanBoxes[instanceIndex] = instanceMeanBox
 
 
+#################################################
 class FrameInfo:
     def __init__(self, bboxes, pos, posMsec, frame):
         self.pos = pos
