24acbf23423c1fcd3dc55798176a72d45bba0df8
trevol <iamvovan@mail.ru>
2019-08-12 15:08:45 +0300

    automatic active area detection. Case of stable scene with 1 pin

--- detection/pins_tracking/v1/PinsWorkArea.py
+++ detection/pins_tracking/v1/PinsWorkArea.py
@@ -9,27 +9,24 @@ from utils.Timer import timeit
 class PinsWorkArea:
     def __init__(self, stableScenePins):
         assert any(stableScenePins)
-        self.__measure(stableScenePins)
+        (self.__contour, self.__meanBoxSize, self.__minPinsDistance,
+         self.__distToAreaThreshold) = self.__measure(stableScenePins)
 
     def draw(self, img, color=Colors.green):
         cv2.drawContours(img, [self.__contour], 0, color, 1)
 
-    def __measure(self, stableScenePins):
-        # TODO: if we have 1 pin????
-        if len(stableScenePins) == 1:  # TODO: if we have 1 pin????
-            raise Exception('len(stableScenePins) == 1')
+    @classmethod
+    def __measure(cls, stableScenePins):
         boxes = [p.box for p in stableScenePins]
-        rawBoxes = [p.box.box for p in stableScenePins]
-        self.__contour = Geometry2D.convexHull(rawBoxes)
-        self.__meanBoxSize = Box.meanSize(boxes)
-        self.__minPinsDistance = self.__computeMinPinsDistance(stableScenePins)
-        self.__distToAreaThreshold = self.__minPinsDistance * 1.5
-
-    @staticmethod
-    def __computeMinPinsDistance(stableScenePins):
-        centers = [p.box.center for p in stableScenePins]
-        minDist = Geometry2D.minL2Distance(centers)
-        return minDist
+        boxesBoxes = [b.box for b in boxes]
+        contour = Geometry2D.convexHull(boxesBoxes)
+        meanBoxSize = Box.meanSize(boxes)
+        if len(stableScenePins) == 1:
+            minPinsDistance = max(*boxes[0].size) * 3
+        else:
+            minPinsDistance = Geometry2D.minL2Distance([b.center for b in boxes])
+        distToAreaThreshold = minPinsDistance * 1.5
+        return contour, meanBoxSize, minPinsDistance, distToAreaThreshold
 
     def inWorkArea(self, boxes):
         boxesInArea = [b for b in boxes if self.__boxInWorkArea(b)]
