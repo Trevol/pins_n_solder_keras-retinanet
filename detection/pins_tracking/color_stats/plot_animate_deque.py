6ee8a6035ed5e6f95ee109aa716d2df135d1fae3
trevol <iamvovan@mail.ru>
2019-08-18 09:44:50 +0300

    VideoPlaybackHandlerBase

--- detection/pins_tracking/color_stats/plot_animate_deque.py
+++ detection/pins_tracking/color_stats/plot_animate_deque.py
@@ -11,12 +11,7 @@ from utils.Timer import timeit
 class images:
     @staticmethod
     def rndIntensities():
-        from_, to = 0, 255
-        current = np.random.randint(from_, to + 1)
-        step = (np.random.random() + .5) * 1.5
-        direction = 1 if np.random.randint(0, 2) == 0 else -1
-        while True:
-            yield roundToInt(current)
+        def next_(current, direction, step):
             current = current + step * direction
             if current <= from_ or current >= to:
                 direction = -direction
@@ -24,6 +19,15 @@ class images:
                     current = from_
                 else:
                     current = to
+            return current, direction
+
+        from_, to = 0, 255
+        current = np.random.randint(from_, to + 1)
+        step = (np.random.random() + .5) * 1.5
+        direction = 1 if np.random.randint(0, 2) == 0 else -1
+        while True:
+            yield roundToInt(current)
+            current, direction = next_(current, direction, step)
 
     def __call__(self):
         channel = self.rndIntensities
@@ -79,6 +83,7 @@ class FramePointColorPlotter:
             self.ax.scatter(self.posData, self.colorData, s=1)
             # plt.pause(.01)
             plt.draw()
+
 
 def main():
     def onClick(e, x, y, flags, _):
