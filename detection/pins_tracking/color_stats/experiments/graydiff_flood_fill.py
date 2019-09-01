2852ba522448ea9e282cf94e37947c87fd8612cb
trevol <iamvovan@mail.ru>
2019-09-01 15:38:12 +0300

    flood filling change map (frame1 - frame2)

--- detection/pins_tracking/color_stats/experiments/graydiff_flood_fill.py
+++ detection/pins_tracking/color_stats/experiments/graydiff_flood_fill.py
@@ -1,25 +1,28 @@
 import cv2
 import numpy as np
 
+from utils.Timer import timeit
+
 
 def putPoint(img, point):
     point2 = point[0] + 1, point[1] + 1
     cv2.rectangle(img, point, point2, 255, -1)
 
 
-def getGrayDiff(blur=True):
+def getGrayDiff(baseThreshold, blur=True):
     grayDiff = cv2.imread('grayDiff_screenshot_30.08.2019.png', cv2.IMREAD_GRAYSCALE)
     if blur:
         grayDiff = cv2.blur(grayDiff, (3, 3))
-    grayDiff = cv2.threshold(grayDiff, 10, 255, cv2.THRESH_TOZERO)[1]
+    grayDiff = cv2.threshold(grayDiff, baseThreshold, 255, cv2.THRESH_TOZERO)[1]
     # cv2.imshow('trunc', grayDiff)
     return grayDiff
 
 
-def floodFill(grayDiff):
-    mask = np.zeros(shape=(grayDiff.shape[0] + 2, grayDiff.shape[1] + 2), dtype='uint8')
+def floodFill(grayDiff, baseThreshold):
+    # mask = np.zeros(shape=(grayDiff.shape[0] + 2, grayDiff.shape[1] + 2), dtype=np.uint8)
+    mask = None
     seedPoint = 886, 77
-    loDiff = 10
+    loDiff = baseThreshold
     upDiff = 255  # 87
     cv2.floodFill(grayDiff, mask, seedPoint, 255, loDiff, upDiff, 8)
     return grayDiff
@@ -33,15 +36,18 @@ def refine(filled):
 
 
 def main():
-    grayDiff = getGrayDiff()
+    baseThreshold = 10
 
-    filled = floodFill(grayDiff.copy())
-    refined = refine(filled)
+    with timeit(autoreport=True):
+        for _ in range(100):
+            grayDiff = getGrayDiff(baseThreshold)
+            filled = floodFill(grayDiff.copy(), baseThreshold)
+            refined = refine(filled)
 
-    cv2.imshow('grayDiff', grayDiff)
-    cv2.imshow('filled', filled)
-    cv2.imshow('refined', refined)
-    cv2.waitKey()
+    # cv2.imshow('grayDiff', grayDiff)
+    # cv2.imshow('filled', filled)
+    # cv2.imshow('refined', refined)
+    # cv2.waitKeyEx()
 
 
 main()
