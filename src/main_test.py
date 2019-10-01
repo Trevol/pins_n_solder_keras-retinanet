f1faaf230f65baa957845341e7738ca59265aac8
trevol <iamvovan@mail.ru>
2019-10-01 12:01:24 +0300

    playing/processing video in separate thread

--- src/main_test.py
+++ src/main_test.py
@@ -1,13 +1,15 @@
+import time
 from time import time, sleep
 
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtGui import QColor, QPalette
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QHBoxLayout, QVBoxLayout, QLabel, QVBoxLayout, \
     QHBoxLayout, QWidget, QDesktopWidget, qApp
 import sys
 import cv2
 
+from utils.VideoPlayback import VideoPlayback
 from widgets.VideoWidget import VideoWidget
 
 
@@ -32,13 +34,34 @@ class Window(QWidget):
 
         self.show()
 
+    def imshow(self, img):
+        self.videoWidget.imshow(img)
 
-if __name__ == '__main__':
+
+class VideoThread(QThread):
+    def __init__(self, videoFile, parent=None):
+        super(VideoThread, self).__init__(parent)
+        self.videoFile = videoFile
+
+    def run(self):
+        video = VideoPlayback(self.videoFile)
+        try:
+            for pos, frame, _ in video.frames():
+                if pos % 100 == 0:
+                    print(pos)
+            print(pos, 'Done!')
+        finally:
+            video.release()
+
+
+def main():
     app = QApplication(sys.argv)
     w = Window()
 
-    img = cv2.imread('segmentation/train/dataset/image/f_3439_229266.67_229.27.jpg')
-    assert img is not None
-    w.videoWidget.imshow(img)
+    thread = VideoThread('/HDD_DATA/Computer_Vision_Task/Video_6.mp4')
+    thread.start()
 
     sys.exit(app.exec())
+
+
+main()
