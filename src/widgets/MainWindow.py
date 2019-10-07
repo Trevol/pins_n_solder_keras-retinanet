35fe8015a5a6b43219f87df0bd2e3f29fafe9d6c
trevol <iamvovan@mail.ru>
2019-10-07 12:21:40 +0300

    start-stop button

--- src/widgets/MainWindow.py
+++ src/widgets/MainWindow.py
@@ -11,11 +11,78 @@ from .VideoWidget import VideoWidget
 
 
 class MainWindow(QMainWindow):
+    class ThreadManager():
+        def __init__(self, parent):
+            self.parent: MainWindow = parent
+            self._trackingThread = None
+            self._playbackThread = None
+            self.trackingStopped = True
+
+        def startOrStop(self):
+            if self._trackingThread:
+                # started!
+                self.stopTracking()
+            else:
+                self.startTracking()
+
+        def stopTracking(self):
+            pass
+
+        def startTracking(self):
+            try:
+                self.parent.startStopButton.setDisabled(True)
+                self.startTechProcessTrackerThread()
+            except:
+                self._trackingThread = None
+                self.parent.startStopButton.setEnabled(True)
+                raise
+
+        def _trackingThreadStarted(self):
+            self.parent.startStopButton.setText('Stop')
+            self.parent.startStopButton.setEnabled(True)
+
+        def _trackingThreadFinished(self):
+            raise NotImplementedError()
+
+        def stopAll(self, waitForCompletion=False):
+            pass
+
+        def __frameInfoReady(self, pos, frame, msec, pinsCount, pinsWithSolderCount, logRecord):
+            self.parent.videoWidget.imshow(frame)
+            self.parent.techProcessInfoWidget.setInfo(pos, msec, pinsCount, pinsWithSolderCount, logRecord)
+
+        def startTechProcessTrackerThread(self):
+            assert self._trackingThread is None
+
+            videoSource = '/HDD_DATA/Computer_Vision_Task/Video_6.mp4'
+            # videoSource = '/HDD_DATA/Computer_Vision_Task/Video_2.mp4'
+            videoSourceDelayMs = 50
+
+            # videoSource = 0
+            # videoSourceDelayMs=-1 #no delay for camera feed
+
+            def techProcessTrackerFactory():
+                pinDetector = PickledDictionaryPinDetector('detection/csv_cache/data/detections_video6.pcl')
+                sceneSegmentation = CachedSceneSegmentation(
+                    '/home/trevol/HDD_DATA/Computer_Vision_Task/frames_6/not_augmented_base_vgg16_more_images_25')
+
+                # pinDetector = RetinanetPinDetector('modelWeights/retinanet_pins_inference.h5')
+                # sceneSegmentation = UnetSceneSegmentation('modelWeights/unet_pins_25_0.000016_1.000000.hdf5')
+
+                techProcessTracker = TechProcessTracker(pinDetector, sceneSegmentation)
+                return techProcessTracker
+
+            self._trackingThread = TechProcessTrackingThread(techProcessTrackerFactory, videoSource, videoSourceDelayMs)
+            self._trackingThread.started.connect(self._trackingThreadStarted)
+            self._trackingThread.finished.connect(self._trackingThreadFinished)
+
+            self._trackingThread.frameInfoReady.connect(self.__frameInfoReady)
+            self._trackingThread.start()
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
-        self.__thread = None
-        self.startTechProcessTrackerThread()
+        self.threadManager = self.ThreadManager(self)
 
     def initUI(self):
         self.setWindowTitle("Process")
@@ -24,7 +91,6 @@ class MainWindow(QMainWindow):
         self.initMainLayout(centralWidget)
 
     def initMainLayout(self, centralWidget: QWidget):
-        # TODO: add start/stop buttons
         layout = QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
 
@@ -50,37 +116,9 @@ class MainWindow(QMainWindow):
             self.close()
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
-        # TODO: stop thread
-        pass
+        self.hide()
+        self.threadManager.stop(waitForCompletion=True)
+        self.close()
 
     def startOrStop(self):
-        print('startOrStop')
-
-    def __frameInfoReady(self, pos, frame, msec, pinsCount, pinsWithSolderCount, logRecord):
-        self.videoWidget.imshow(frame)
-        self.techProcessInfoWidget.setInfo(pos, msec, pinsCount, pinsWithSolderCount, logRecord)
-
-    def startTechProcessTrackerThread(self):
-        assert self.__thread is None
-
-        videoSource = '/HDD_DATA/Computer_Vision_Task/Video_6.mp4'
-        # videoSource = '/HDD_DATA/Computer_Vision_Task/Video_2.mp4'
-        videoSourceDelayMs = 50
-
-        # videoSource = 0
-        # videoSourceDelayMs=-1 #no delay for camera feed
-
-        def techProcessTrackerFactory():
-            pinDetector = PickledDictionaryPinDetector('detection/csv_cache/data/detections_video6.pcl')
-            sceneSegmentation = CachedSceneSegmentation(
-                '/home/trevol/HDD_DATA/Computer_Vision_Task/frames_6/not_augmented_base_vgg16_more_images_25')
-
-            # pinDetector = RetinanetPinDetector('modelWeights/retinanet_pins_inference.h5')
-            # sceneSegmentation = UnetSceneSegmentation('modelWeights/unet_pins_25_0.000016_1.000000.hdf5')
-
-            techProcessTracker = TechProcessTracker(pinDetector, sceneSegmentation)
-            return techProcessTracker
-
-        self.__thread = TechProcessTrackingThread(techProcessTrackerFactory, videoSource, videoSourceDelayMs)
-        self.__thread.frameInfoReady.connect(self.__frameInfoReady)
-        self.__thread.start()
+        self.threadManager.startOrStop()
