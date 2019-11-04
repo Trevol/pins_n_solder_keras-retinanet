be9204edde178acc0b6c4815440fbae0bbed7f3c
trevol <iamvovan@mail.ru>
2019-11-04 15:03:25 +0300

    iou...

--- src/dead_stuff/intersection_over_union/IoU.py
+++ src/dead_stuff/intersection_over_union/IoU.py
@@ -6,7 +6,12 @@ from utils.Timer import timeit
 
 class Box:
     class Union:
-        pass
+        def __init__(self, box1, box2):
+            self.box1 = box1
+            self.box2 = box2
+
+        def area(self, intersectionArea):
+            return self.box1.area() + self.box2.area() - intersectionArea
 
     class Intersection:
         def __init__(self, box1, box2):
@@ -39,17 +44,29 @@ class Box:
         def box(self):
             vertices = self.vertices()
             # TODO: analyze points and choose p00 and p11
-            return None
+            if len(vertices) <= 1:
+                return None
+            assert len(vertices) == 4
+            xx = [p.x for p in vertices]
+            yy = [p.y for p in vertices]
+            x0 = min(xx)
+            y0 = min(yy)
+            x1 = max(xx)
+            y1 = max(yy)
+            return Box(Point(x0, y0), Point(x1, y1))
 
-        def area(self, other):
+        def area(self):
             box = self.box()
             if box is None:
                 return 0
-            box.area()
+            return box.area()
 
     def __init__(self, p0, p1):
-        self.p00 = Point.convert(p0)
-        self.p11 = Point.convert(p1)
+        p0 = Point.convert(p0)
+        p1 = Point.convert(p1)
+        assert p0.x < p1.x and p0.y < p1.y
+        self.p00 = p0
+        self.p11 = p1
         self.p01 = Point(self.p00.x, self.p11.y)
         self.p10 = Point(self.p11.x, self.p00.y)
         self.vertices = [self.p00, self.p11, self.p01, self.p10]
@@ -71,28 +88,42 @@ class Box:
         return self.Intersection(self, other)
 
     def union(self, other):
-        return 1
+        return self.Union(self, other)
 
     def iou(self, other):
-        return self.intersection(self, other).area() / self.union(other).area()
+        intersectionArea = self.intersection(other).area()
+        return intersectionArea / self.union(other).area(intersectionArea)
 
 
 class Edge:
+    @staticmethod
+    def __sort(p0, p1):
+        if p0.x < p1.x or p0.y < p1.y:
+            return p0, p1
+        return p1, p0
+
     def __init__(self, p0, p1):
-        self.p0 = p0
-        self.p1 = p1
+        self.p0, self.p1 = self.__sort(p0, p1)
 
     def intersection(self, other):
         edgesArePerpendicular = self.p0.x == self.p1.x and other.p0.y == other.p1.y or \
                                 self.p0.y == self.p1.y and other.p0.x == other.p1.x
         if not edgesArePerpendicular:
-            return None
-        # x-coord of intersection: where p0.x == p1.x
-        xCoord = self.p0.x if self.p0.x == self.p1.x else other.p0.x
-        # y-coord of intersection: where p0.y == p1.y
-        yCoord = self.p0.y if self.p0.y == self.p1.y else other.p0.y
-        print(xCoord, yCoord)
-        return Point(1, 1)
+            return
+
+        if self.p0.x == self.p1.x:
+            xEdge = self
+            yEdge = other
+        else:
+            xEdge = other
+            yEdge = self
+
+        x = xEdge.p0.x
+        y = yEdge.p0.y
+        if x < yEdge.p0.x or x > yEdge.p1.x or y < xEdge.p0.y or y > xEdge.p1.y:  #
+            return
+
+        return Point(x, y)
 
 
 class Point:
@@ -115,18 +146,66 @@ class Point:
 
     def __eq__(self, other):
         # if not isinstance(other, Point): return False
-        return self.x == other.x and self.y == other.y
+        return other and self.x == other.x and self.y == other.y
+
+
+def edgeIntersectionTest():
+    e1 = Edge(Point(1, 4), Point(6, 4))
+    e2 = Edge(Point(5, 5), Point(5, 1))
+    assert e1.intersection(e2) == Point(5, 4)
+    assert e2.intersection(e1) == Point(5, 4)
+
+    e1 = Edge(Point(1, 4), Point(6, 4))
+    e2 = Edge(Point(6, 5), Point(6, 1))
+    assert e1.intersection(e2) == Point(6, 4)
+    assert e2.intersection(e1) == Point(6, 4)
+
+    e1 = Edge(Point(1, 4), Point(6, 4))
+    e2 = Edge(Point(1, 5), Point(1, 1))
+    assert e1.intersection(e2) == Point(1, 4)
+    assert e2.intersection(e1) == Point(1, 4)
+
+    e1 = Edge(Point(1, 4), Point(6, 4))
+    e2 = Edge(Point(7, 5), Point(7, 1))
+    assert e1.intersection(e2) is None
+    assert e2.intersection(e1) is None
+
+    e1 = Edge(Point(1, 4), Point(6, 4))
+    e2 = Edge(Point(-1, 5), Point(-1, 1))
+    assert e1.intersection(e2) is None
+    assert e2.intersection(e1) is None
 
 
 def main():
-    e1 = Edge(Point(1, 4), Point(6, 4))
-    e2 = Edge(Point(5, 5), Point(5, 1))
-    e1.intersection(e2)
-    e2.intersection(e1)
-    # b1 = Box((2, 1), (8, 6))
-    # b2 = Box((5, -1), (7, 2))
-    # print(b1.intersection(b2).vertices())
-    # assert b1.intersection(b2) == 2
+    b1 = Box((2, 1), (8, 6))
+    b2 = Box((5, -1), (7, 2))
+    assert b1.intersection(b2).vertices() == {Point(5, 1), Point(5, 2), Point(7, 2), Point(7, 1)}
+    assert b1.intersection(b2).area() == 2
+    assert b1.iou(b2) == 2 / 34
+
+
+    b1 = Box((2, 1), (8, 6))
+    b2 = Box((4, 3), (5, 4))
+    assert b1.intersection(b2).vertices() == {Point(4, 3), Point(5, 4), Point(5, 3), Point(4, 4)}
+    assert b1.intersection(b2).area() == 1
+    assert b1.iou(b2) == 1 / 30
+
+    b1 = Box((2, 1), (8, 6))
+    b2 = Box((2, 5), (3, 6))
+    assert b1.intersection(b2).area() == 1
+    assert b1.iou(b2) == 1 / 30
+
+    b1 = Box((2, 1), (8, 6))
+    b2 = Box((1, 6), (2, 7))
+    assert b1.intersection(b2).vertices() == {Point(2, 6)}
+    assert b1.intersection(b2).area() == 0
+    assert b1.iou(b2) == 0
+
+    b1 = Box((2, 1), (8, 6))
+    b2 = Box((1, 7), (2, 8))
+    assert b1.intersection(b2).vertices() == set()
+    assert b1.intersection(b2).area() == 0
+    assert b1.iou(b2) == 0
 
 
 main()
